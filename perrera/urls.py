0ced90c036def4a15ac91ebfd6dbb27d7474b16f
cris <cr.barrales@uandresbello.edu>
2017-07-05 14:17:25 -0400

    agregando usuario

--- perrera/urls.py
+++ perrera/urls.py
@@ -2,11 +2,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.contrib.auth.views import login
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^mascota/', include('mascota.urls', namespace='mascota')),
-    url(r'^usuario/', include('usuario.urls', namespace='usuario')),
     url(r'^adopcion/', include('adopcion.urls', namespace='adopcion')),
+    url(r'^usuario/', include('usuario.urls', namespace='usuario')),
+    url(r'^$', login, {'template_name':'index.html'}, name='login'),
+    
 
 ]
