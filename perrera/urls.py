5c0348aba6f3ed1def0c48b70e9199d2f44b207b
cris <cr.barrales@uandresbello.edu>
2017-07-05 13:15:09 -0400

    creando usuarios

--- perrera/urls.py
+++ perrera/urls.py
@@ -2,9 +2,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^mascota/', include('mascota.urls', namespace='mascota')),
+    url(r'^usuario/', include('usuario.urls', namespace='usuario')),
     url(r'^adopcion/', include('adopcion.urls', namespace='adopcion')),
 
 ]
